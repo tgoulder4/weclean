427fa642ea3dc6558d9775ccf5c6bcc7e569c785
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-12-25 04:55:47 +0000

    updated perk icons

--- components/Screens/GoProScreen.tsx
+++ components/Screens/GoProScreen.tsx
@@ -11,8 +11,7 @@ import ProPerk from './GoPro/ProPerk';
 const GoProScreen = () => {
     return (
         <View className='flex flex-col h-full flex-1'>
-            {/* scrollview is bg-[#080808] */}
-            <ScrollView className='bg-yellow-500 border-red-500 border-2 flex-1  gap-y-4'>
+            <ScrollView className=' bg-[#080808] border-2 flex-1  gap-y-4'>
                 <View className='px-2 pt-28'>
 
 
@@ -25,10 +24,10 @@ const GoProScreen = () => {
                     </Text>
                     <Pod backgroundColour={`[${foregroundColour}]`} noStroke={true} >
                         <View className='flex flex-col'>
-                            <ProPerk perkTitle='Monthly Cleaning Supplies 🧼' perkText="Get regular cleaning supplies on us" />
-                            <ProPerk perkTitle='Automated Rota 📅' perkText="Create a rota in seconds, instantly putting your team into action" />
-                            <ProPerk perkTitle='Entry into Pro-Only Competitions 💸' perkText="Win exclusive cash prizes in competitions of cleanliness" />
-                            <ProPerk perkTitle="Join multiple crews" perkText="Join multiple Crews" />
+                            <ProPerk perkIcon='🧼' perkTitle='Monthly Cleaning Supplies' perkText="Get regular cleaning supplies on us" />
+                            <ProPerk perkIcon='📅' perkTitle='Automated Rota' perkText="Create a rota in seconds, instantly putting your team into action" />
+                            <ProPerk perkIcon='💸' perkTitle='Entry into Pro-Only Competitions' perkText="Win exclusive cash prizes in competitions of cleanliness" />
+                            <ProPerk perkIcon='' perkTitle="Join multiple crews" perkText="Join multiple Crews" />
                         </View>
                     </Pod>
                     <Text className='font-afa text-center text-white mb-[20px]'>
