91329a7a8873d99286b07a0c66e5df40412e3df7
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-12-25 02:46:24 +0000

    before react-modal-sheet

--- components/Screens/GoPro/ProPerk.tsx
+++ components/Screens/GoPro/ProPerk.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const ProPerk = (props: { perkTitle: string, perkText: string }) => {
+const ProPerk = (props: { perkTitle: string, perkText: string, perkIcon: string }) => {
     return (
-        <View className='bg-indigo-500 flex-1 flex flex-row gap-x-2 mb-[20px]'>
-            <Text className='font-afa text-white'>✓</Text>
+        <View className='flex-1 flex flex-row gap-x-2 mb-[20px]'>
+            <Text className='font-afa text-white'>{props.perkIcon}</Text>
             <View className='flex-1 flex flex-col gap-y-2'>
                 <Text className='font-afaB text-white'>{props.perkTitle}</Text>
                 <Text className='font-afa text-white flex-1'>{props.perkText}</Text>
